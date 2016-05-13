38606a2f8944705f30a88e220dbd75bf411f1827
chad <eesql@foxmail.com>
2016-05-13 17:50:57 +0800

    update test case

--- src/test/scala/TitanicSuite.scala
+++ src/test/scala/TitanicSuite.scala
@@ -1,5 +1,7 @@
 import org.apache.spark.{SparkConf, SparkContext}
 import org.scalatest.FunSuite
+import TitanicPredict.normData
+import TitanicPredict.swapData
 
 /**
   * Created by elainetuang on 5/12/16.
@@ -12,12 +14,17 @@ class TitanicSuite extends FunSuite {
 
   test("transform fields test") {
     val testString = "male"
-    assert(TitanicPredict.transFields(testString) == "1")
+    assert(TitanicPredict.transFields(testString) == "0")
   }
 
   test("spark context test") {
     val conf = new SparkConf().setAppName("TitanicPredictionExample")
+    conf.setMaster("local[2]")
     val sc   = new SparkContext(conf)
+
+    val trainData = normData( swapData( sc.textFile("resources/train.csv") ) )
+
+    assert( trainData.count() == 10000 )
 
     sc.stop()
   }
